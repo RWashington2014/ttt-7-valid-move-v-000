45eb5578daf6801ec6f90b778dc245b41fbb9422
Robert W <robwash@gmail.com>
2018-01-18 18:16:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,9 +6,8 @@ def valid_move?(board, index)
 
   if  index > 8 || position_taken?(board, index)
     false
-  #end
-  #if !(position_taken?(board, index))
-    else  true
+  else
+    true
   end
 
 end
