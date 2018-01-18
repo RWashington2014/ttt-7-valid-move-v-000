18156c370748f663b7974cfa0cf0ba1be6abe1ee
Robert W <robwash@gmail.com>
2018-01-18 17:18:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -18,7 +18,9 @@ def position_taken?(board, index)
 
   if board[index] ==  " " || board[index] == "" || board[index] == nil
     false
-  else board[index] == "X" || board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
+  else
+    nil
   end
 end
